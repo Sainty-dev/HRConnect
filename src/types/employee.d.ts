38a40918871c54a8dc4787209303edac2e2a6132
saintympheroane <Kwena.Mpheroane@boxfusion.io>
2024-06-18 03:48:04 +0200

    chore: add picker type

--- src/types/employee.d.ts
+++ src/types/employee.d.ts
@@ -1,3 +1,5 @@
-import {TextFieldProps} from 'react-native-ui-lib';
+import {TextFieldProps, PickerProps} from 'react-native-ui-lib';
 import {TextInputBaseProps} from '../components/common/input/interface';
+import {ItemPickerBaseProps} from '../components/common/picker/interface';
 export type TextInputProps = TextInputBaseProps & TextFieldProps;
+export type ItemPickerProps = PickerProps & ItemPickerBaseProps;
